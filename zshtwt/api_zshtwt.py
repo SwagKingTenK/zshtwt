92056da95f53f062f505b6678af8edb33d031a7d
Colby Cox <uoflcards22@gmail.com>
2015-10-08 00:04:27 -0400

    Deletions from the api as I transition the project.

--- zshtwt/api_zshtwt.py
+++ zshtwt/api_zshtwt.py
@@ -4,22 +4,6 @@ import time
 
 API_URL = 'https://api.twitter.com/1.1/'
 
-def home_timeline(count=20):
-	URL = API_URL+'statuses/home_timeline.json'
-	data = json.loads(requests.get(URL, auth=Auth).text)
-	for x in range(len(data)):
-		print(data[x]['user']['name']+' : '+data[x]['text'])
-
-def user_timeline(count=20, screen_name=User.screen_name):
-	URL = API_URL+'statuses/user_timeline.json'
-	data = json.loads(requests.get(URL, auth=Auth, params=dict(count=count, screen_name=screen_name)).text)
-	for x in range(len(data)):
-		print(data[x]['user']['name']+' : '+data[x]['text'])
-
-def tweet(status='Tweeting from @zshtwt at: '+str(time.time()), in_reply_to=None):
-	URL = API_URL+'statuses/update.json'
-	requests.post(url=URL, auth=Auth, params=dict(status=status, in_reply_to=in_reply_to))
-	return  
 def get_followers(screen_name=User.screen_name, count=50):
 	URL=API_URL+'followers/list.json'
 	data = json.loads(requests.get(URL, auth=Auth, params=dict(screen_name=User.screen_name, count=count)).text)
