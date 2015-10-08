ae7227b244ce82f08ddee42658509a4a24c3c127
Colby Cox <uoflcards22@gmail.com>
2015-10-08 00:00:08 -0400

    Deletions from the api as I transition the project.

--- zshtwt/api_zshtwt.py
+++ zshtwt/api_zshtwt.py
@@ -20,4 +20,30 @@ def tweet(status='Tweeting from @zshtwt at: '+str(time.time()), in_reply_to=None
 	URL = API_URL+'statuses/update.json'
 	requests.post(url=URL, auth=Auth, params=dict(status=status, in_reply_to=in_reply_to))
 	return  
+def get_followers(screen_name=User.screen_name, count=50):
+	URL=API_URL+'followers/list.json'
+	data = json.loads(requests.get(URL, auth=Auth, params=dict(screen_name=User.screen_name, count=count)).text)
+	x=0
+	try:
+		while data['next_cursor'] != 0:
+			for x in range(len(data['users'])):
+				print(data['users'][x]['screen_name'])
+				x+=1
+			data = json.loads(requests.get(URL, auth=Auth, params=dict(screen_name=User.screen_name, count=count, cursor=data['next_cursor'])).text)
+		print(x)
+	except:
+		pass
+def get_following(screen_name=User.screen_name, count=50):
+	URL=API_URL+'friends/list.json'
+	data = json.loads(requests.get(URL, auth=Auth, params=dict(screen_name=User.screen_name, count=count)).text)
+	x=0
+	try:
+		while data['next_cursor'] != 0:
+			for x in range(len(data['users'])):
+				print(data['users'][x]['screen_name'])
+				x+=1
+			data = json.loads(requests.get(URL, auth=Auth, params=dict(screen_name=User.screen_name, count=count, cursor=data['next_cursor'])).text)
+		print(x)
+	except:
+		pass
 
